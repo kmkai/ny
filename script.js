5a2aaa5bdee653311453656b3a45bb794897b661
kmkai <153850711+kmkai@users.noreply.github.com>
2024-02-26 03:42:04 +0100

    All new files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("toplayer").style.display = "flex";
+  }, 1000); // Vis etter 10 sekunder
+});
